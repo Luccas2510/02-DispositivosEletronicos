9893edeb53c480d47f27a0fe98e7acc02582440f
Luccas2510 <luccas.f.santos@aluno.senai.br>
2024-11-12 14:00:03 -0300

    Adicionar arquivos de projeto.

---
+++ 02-DispositivosEletronicos/Controllers/DispositivoController.cs
@@ -0,0 +1,19 @@
+﻿using _02_DispositivosEletronicos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_DispositivosEletronicos.Controllers
+{
+    public class DispositivoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<DispositivoEletronico> dispositivos = new List<DispositivoEletronico>
+            {
+                new Computador { Marca = "Dell", Modelo = "Inspirion", AnoDeFabricacao = 2021, EhLaptop = true, MemoriaRam = 16, SistemaOperacional = "Windows 11"},
+                new Computador { Marca = "HP", Modelo = "Pavilion", AnoDeFabricacao = 2019, EhLaptop = false, MemoriaRam = 8 , SistemaOperacional = "Windows 11"},
+                new Smartphone { Marca = "Apple", Modelo = "Iphone", AnoDeFabricacao = 2022, Tem5G = true}
+            };
+            return View(dispositivos);
+        }
+    }
+}
---
+++ 02-DispositivosEletronicos/Models/DispositivoEletronico.cs
@@ -0,0 +1,11 @@
+﻿namespace _02_DispositivosEletronicos.Models
+{
+    public abstract class DispositivoEletronico
+    {
+        public string Marca { get; set; } //get: obter set: dar novos valores setar novos valores
+        public string Modelo { get; set;}
+        public int AnoDeFabricacao { get; set;}
+        //Método
+        public abstract string ExibirDetalhes();
+    }
+}
